ce0baf4f12299eace2b9dd364f955c5332199efe
Yogesh Seralia <yogeshseralia21@gmail.com>
2021-11-18 23:17:32 +0530

    theme setting.

--- app/src/main/java/com/funzilla/movietime/MainActivity.kt
+++ app/src/main/java/com/funzilla/movietime/MainActivity.kt
@@ -3,6 +3,9 @@ package com.funzilla.movietime
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+/**
+ * https://www.behance.net/gallery/86572373/Must-Movie-App?tracking_source=search_projects_recommended%7Cmovie%20mobile%20app
+ */
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
